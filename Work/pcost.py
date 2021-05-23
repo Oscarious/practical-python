79ff77c96d421c0981fba8e44f11c4071bc54cd6
zhangqingyu <zhangqingyu10@meituan.com>
2021-05-23 16:43:57 +0800

    ex1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+total_cost = 0
+for line in f:
+    row = line.split(',')
+    try:
+        total_cost += int(row[1]) * float(row[2])
+    except ValueError as e:
+        pass
+print(f'total cost: {total_cost}')
