fa7df7202328fe07b1cdd24deb5f5328561a70c8
zhangqingyu <zhangqingyu10@meituan.com>
2021-05-23 16:03:57 +0800

    ex1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,14 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+extra_payment_first_year = 1000
+mounth = 1
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
+    principal = principal * (1+rate/12) - payment - extra_payment_first_year
     total_paid = total_paid + payment
+    mounth += 1
+    if mounth > 12:
+        extra_payment_first_year = 0
 
-print('Total paid', total_paid)
+print('Total paid:', total_paid, ' over:', mounth, 'months')
