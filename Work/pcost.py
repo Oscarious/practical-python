f3445efe3b277d6aade738ff37728f4b4f32ad5a
zhangqingyu <zhangqingyu10@meituan.com>
2021-05-23 16:54:01 +0800

    ex1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-total_cost = 0
-for line in f:
-    row = line.split(',')
-    try:
-        total_cost += int(row[1]) * float(row[2])
-    except ValueError as e:
-        pass
+
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    total_cost = 0
+    for line in f:
+        row = line.split(',')
+        try:
+            total_cost += int(row[1]) * float(row[2])
+        except ValueError as e:
+            pass
+    return total_cost
+total_cost = portfolio_cost('Data/portfolio.csv')
 print(f'total cost: {total_cost}')
