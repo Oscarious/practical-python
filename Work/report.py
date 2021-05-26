bedf473fcba63cfd962fce22d0f466d2c4d0fbed
zhangqingyu <zhangqingyu10@meituan.com>
2021-05-26 23:34:28 +0800

    2.6

--- Work/report.py
+++ Work/report.py
@@ -15,3 +15,12 @@ def read_portfolio(filename):
                 holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
                 portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    res = {}
+    with open('Data/prices.csv', 'r') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if (len(row) > 1):
+                res[row[0]] = float(row[1])
+    return res
