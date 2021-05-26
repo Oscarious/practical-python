2a83986f36eb39af1642167e4bbb682c41cb0de4
zhangqingyu <zhangqingyu10@meituan.com>
2021-05-26 23:16:28 +0800

    ex2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,23 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                nshares = int(row[1])
+                price = float(row[2])
+                holding = (row[0], int(row[1]), float(row[2]))
+                portfolio.append(holding)
+    return portfolio
+
+# if __name__ == '__main__':
+#     if len(sys.argv) < 2:
+#         print("error")
+#         exit()
+#     read_partfolio(sys.argv[1])
