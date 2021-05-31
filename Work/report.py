e62fc9bd4d646e06646ec25d0b3759bc49a5c205
zhangqingyu10 <zhangqingyu10@meituan.com>
2021-05-31 13:21:16 +0800

    ex3.2

--- Work/report.py
+++ Work/report.py
@@ -47,3 +47,9 @@ def print_report(report):
     print('%10s %10s %10s %10s' % split_line)
     for r in report:
         print('%10s %10d %10.2f %10.2f' % r)
+
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
