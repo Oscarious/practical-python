41e74c0db09733c63e5bc10fad87ac238ab3f727
zhangqingyu10 <zhangqingyu10@meituan.com>
2021-05-31 17:08:24 +0800

    ex3.9

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -17,22 +17,24 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
 
         records = []
         for row in rows:
-            if not row:     # Skip rows with no data
-                continue
+            try:
+                if not row:     # Skip rows with no data
+                    continue
 
-            # If specific column indices are selected, pick them out
-            if select:
-                row = [ row[index] for index in indices]
+                # If specific column indices are selected, pick them out
+                if select:
+                    row = [ row[index] for index in indices]
 
-            # Apply type conversion to the row
-            if types:
-                row = [func(val) for func, val in zip(types, row)]
+                # Apply type conversion to the row
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
 
-            # Make a dictionary or a tuple
-            if headers:
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
-
+                # Make a dictionary or a tuple
+                if headers:
+                    record = dict(zip(headers, row))
+                else:
+                    record = tuple(row)
+                records.append(record)
+            except ValueError as e:
+                print(e)
         return records
