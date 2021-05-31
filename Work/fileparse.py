21b611532216ba866e5144c19db15073dc6286c1
zhangqingyu10 <zhangqingyu10@meituan.com>
2021-05-31 18:59:44 +0800

    ex3.18

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,43 +2,42 @@
 import csv
 import report
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     if select and has_headers == False:
         raise RuntimeError("select argument requires column headers")
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
 
-        headers = next(rows) if has_headers else []
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        # If specific columns have been selected, make indices for filtering
-        if select:
-            indices = [ headers.index(colname) for colname in select ]
-            print(colname)
-            headers = select
+    headers = next(rows) if has_headers else []
 
-        records = []
-        for rowno, row in enumerate(rows, 1):
-            try:
-                if not row:     # Skip rows with no data
-                    continue
+    # If specific columns have been selected, make indices for filtering
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
 
-                # If specific column indices are selected, pick them out
-                if select:
-                    row = [ row[index] for index in indices]
-
-                # Apply type conversion to the row
-                if types:
-                    row = [func(val) for func, val in zip(types, row)]
-
-                # Make a dictionary or a tuple
-                if headers:
-                    record = dict(zip(headers, row))
-                else:
-                    record = tuple(row)
-                records.append(record)
-            except ValueError as e:
-                if not silence_errors:
-                    print("Row {}: Couldn't convert {}".format(rowno, row))
-                    print("Row {}: Reason {}".format(rowno, e))
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        try:
+            if not row:     # Skip rows with no data
                 continue
+
+            # If specific column indices are selected, pick them out
+            if select:
+                row = [ row[index] for index in indices]
+
+            # Apply type conversion to the row
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            # Make a dictionary or a tuple
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+        except ValueError as e:
+            if not silence_errors:
+                print("Row {}: Couldn't convert {}".format(rowno, row))
+                print("Row {}: Reason {}".format(rowno, e))
+            continue
         return records
--- Work/report.py
+++ Work/report.py
@@ -3,27 +3,15 @@
 # Exercise 2.4
 import csv
 import sys
+import fileparse
 
 def read_portfolio(filename):
-    portfolio = []
-    with open(filename, 'rt') as f:
-            rows = csv.reader(f)
-            headers = next(rows)
-            for row in rows:
-                nshares = int(row[1])
-                price = float(row[2])
-                holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
-                portfolio.append(holding)
-    return portfolio
+    with open(filename) as lines:
+        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
-    res = {}
-    with open('Data/prices.csv', 'r') as f:
-        rows = csv.reader(f)
-        for row in rows:
-            if (len(row) > 1):
-                res[row[0]] = float(row[1])
-    return res
+    with open(filename) as lines:
+        return dict(fileparse.parse_csv(lines, types=[str,float], has_headers=False))
 
 def cal_profit():
     portfolios = read_portfolio('Data/portfolio.csv')
